0e1c2d63d601202ec9d6100b703209602e949e45
Azani David Pinkney <info@azani.org>
2023-10-18 20:53:09 -0700

    DOM creating lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.querySelector("main#main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Azani is the champion";
+
+document.body.append(newHeader);
